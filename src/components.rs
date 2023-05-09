981a07e41e5c0e580abe49e77847d8c174093211
Laxsjo <rasmus.soderhielm@gmail.com>
2023-05-09 21:32:04 +0200

    setup basic color picker mouse controls

--- src/components.rs
+++ src/components.rs
@@ -1,4 +1,4 @@
-use leptos::*;
+use leptos::{ev::MouseEvent, *};
 use leptos_router::*;
 
 #[component]
@@ -17,10 +17,31 @@ pub fn RouteLink(cx: Scope, route_name: &'static str, children: Children) -> imp
 
 #[component]
 pub fn ColorPicker(cx: Scope) -> impl IntoView {
+    let (pos_x, set_pos_x) = create_signal(cx, 0.);
+    let (pos_y, set_pos_y) = create_signal(cx, 0.);
+
+    let on_mouse_move = move |ev: MouseEvent| {
+        // ev.client_x();
+        let Some(element) = ev.target() else {
+            return;
+        };
+
+        log!("move {}, {}", ev.offset_x(), ev.offset_x());
+    };
+
     view! { cx,
         <div class="color-picker">
-            <div class="color-picker__preview">
-                <div></div>
+
+            <div
+                class="color-picker__map"
+                on:mousemove=on_mouse_move
+            >
+                <div class="color-picker__color">
+                    <div class="color-picker__color__cursor"/>
+                </div>
+                <div class="color-picker__hue">
+                    <div class="color-picker__hue__cursor"/>
+                </div>
             </div>
         </div>
     }
